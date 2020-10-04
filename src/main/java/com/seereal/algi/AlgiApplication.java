1dd0b73e191651bdddd95bc17820d163db1d85a2
YooHyuk Lee <azsxd34@naver.com>
2020-10-05 02:21:47 +0900

    init projet

---
+++ src/main/java/com/seereal/algi/AlgiApplication.java
@@ -0,0 +1,13 @@
+package com.seereal.algi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seereal/algi/AlgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seereal.algi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
