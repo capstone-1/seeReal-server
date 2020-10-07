ebb43f7910581045a4ebd7e7ef773fa84338cea0
Seongpyo Hong <sphong0417@gmail.com>
2020-10-07 23:00:06 +0900

    (fixed) Extract Skip Urls

--- src/main/java/com/seereal/algi/security/SecurityConstants.java
+++ src/main/java/com/seereal/algi/security/SecurityConstants.java
@@ -1,10 +1,14 @@
 package com.seereal.algi.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 900_000; //15 mins
     public static final String HEADER_STRING = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
-
+    public static final List<String> SKIP_URLS = Arrays.asList("/organization/login", "/organization/signup","/organization/find/register-number-by-email");
     //TODO: Change To application.properties
     public static String SECRET_KEY = "jwttest";
     public static String ISSUER = "capstone";
