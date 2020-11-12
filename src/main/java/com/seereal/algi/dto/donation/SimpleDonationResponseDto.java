6362789df579f85e71e779c032636eaaff0e5c21
YooHyuk Lee <azsxd34@naver.com>
2020-11-12 20:18:34 +0900

    fix simpleresponseDto

--- src/main/java/com/seereal/algi/dto/donation/SimpleDonationResponseDto.java
+++ src/main/java/com/seereal/algi/dto/donation/SimpleDonationResponseDto.java
@@ -1,8 +1,13 @@
 package com.seereal.algi.dto.donation;
 
+import com.seereal.algi.model.category.Category;
 import com.seereal.algi.model.donation.Donation;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -10,10 +15,18 @@ public class SimpleDonationResponseDto {
     private Long id;
     private String name;
     private String registrant;
+    private String shortIntroduction;
+    private String profileUrl;
+    private String target;
+    private List<String> categories;
 
     public SimpleDonationResponseDto(Donation donation) {
         this.id = donation.getId();
         this.name = donation.getName();
         this.registrant = donation.getRegistrant();
+        this.shortIntroduction = donation.getShortIntroduction();
+        this.profileUrl = donation.getProfileUrl();
+        this.target = donation.getTarget();
+        this.categories = donation.getCategories().stream().map(Category::getName).collect(Collectors.toList());
     }
 }
